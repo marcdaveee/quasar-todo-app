9d499f26f57c86b471773998689dbca765dc4282
Marc Dave <davevalencia17@gmail.com>
2024-02-21 22:50:54 +0800

    Added Dashboard.vue

--- src/router/routes.js
+++ src/router/routes.js
@@ -38,6 +38,11 @@ const routes = [
             component: () => import("../pages/UserManagement/Users.vue"),
           },
           {
+            path: "dashboard",
+            name: "dashboard",
+            component: () => import("../pages/Onboarding/Dashboard.vue"),
+          },
+          {
             path: "or-numbers",
             name: "or-numbers",
             component: () => import("../pages/ORNumbers/ORNumbers.vue"),
